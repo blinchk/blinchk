c019c95665759e3e9f9c04526c7047adf0d1a8a7
Nikolas Laus <49353043+blinchk@users.noreply.github.com>
2020-11-23 19:59:21 +0200

    Add responsibility to particles.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
-particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+if (window.screen.width > 700) {
+    particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
+        console.log('callback - particles.js config loaded');
+      });
+}

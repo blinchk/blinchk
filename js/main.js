c5b3ee663b6bc56f0d7cbdb6fb6a882d03bdef0f
Nikolas Laus <49353043+blinchk@users.noreply.github.com>
2020-11-23 20:13:50 +0200

    Replace img/svg and particles config to assets.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 if (window.screen.width > 700) {
-    particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
+    particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
         console.log('callback - particles.js config loaded');
       });
 }
